aeed4cafd603acc8c20e9913e2c22a743fdd67b9
AlexPoddubny <usatij.alex@gmail.com>
2020-02-04 21:10:55 +0200

    Content

--- app/Http/Controllers/Admin/Core.php
+++ app/Http/Controllers/Admin/Core.php
@@ -1,27 +1,30 @@
 <?php
-
-namespace App\Http\Controllers\Admin;
-
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-
-class Core extends Controller
-{
-
-    public function __construct() {
-		//$this->middleware('mymiddle');
-	}
-    
-    //list materials
-    public function getArticles() {
-		
-	}
 	
-	 //material
-    public function getArticle($id) {
+	namespace App\Http\Controllers\Admin;
+	
+	use Illuminate\Http\Request;
+	
+	use App\Http\Requests;
+	use App\Http\Controllers\Controller;
+	
+	class Core extends Controller
+	{
 		
-		echo 'Ответ - '.$id;
+		public function __construct()
+		{
+			//$this->middleware('mymiddle');
+		}
+		
+		//list materials
+		public function getArticles()
+		{
+		
+		}
+		
+		//material
+		public function getArticle($id)
+		{
+			
+			echo 'Ответ - ' . $id;
+		}
 	}
-}
--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -1,19 +1,41 @@
 <?php
-
-namespace App\Http\Controllers\Admin;
-
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-
-class IndexController extends Controller
-{
-    public function show() {
-		
-		if(view()->exists('default.index')) {
-			return view('default.index')->withTitle('Hello World');
+	
+	namespace App\Http\Controllers\Admin;
+	
+	use Illuminate\Http\Request;
+	
+	use App\Http\Requests;
+	use App\Http\Controllers\Controller;
+	
+	class IndexController extends Controller
+	{
+		public function show()
+		{
+			$array = [
+				'title' => 'Laravel Project',
+				'data' => [
+					'one' => 'List 1',
+					'two' => 'List 2',
+					'three' => 'List 3',
+					'four' => 'List 4',
+					'five' => 'List 5'
+				],
+				'dataI' => [
+					'List 1',
+					'List 2',
+					'List 3',
+					'List 4',
+					'List 5'
+				],
+				'bvar' => true,
+				'script' => "<script>alert('hello')</script>"
+			];
+			
+			
+			
+			if (view()->exists('default.index')) {
+				return view('default.index', $array);
+			}
+			abort(404);
 		}
-		abort(404);
 	}
-}
