7c0fb3164e8a23d0f27078ef6066d456ca4386b4
AlexPoddubny <usatij.alex@gmail.com>
2020-02-06 16:57:14 +0200

    Seeder

---
+++ database/seeds/ArticlesSeeder.php
@@ -0,0 +1,43 @@
+<?php
+	
+	use App\Article;
+	use Illuminate\Database\Seeder;
+
+class ArticlesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //method 1
+	    DB::insert('INSERT INTO `articles` (`name`, `text`, `img`) VALUES (?, ?, ?)',
+		    [
+		    	'Blog post',
+			    '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
+			    'pic1.jpg'
+		    ]
+	    );
+	    //method 2
+	    DB::table('articles')->insert([
+	    	[
+	    		'name' => 'Blog post 2',
+			    'text' => '<p>2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
+			    'img' => 'pic2.jpg'
+		    ],
+		    [
+	    		'name' => 'Blog post 3',
+			    'text' => '<p>3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
+			    'img' => 'pic3.jpg'
+		    ],
+	    ]);
+	    //method 3
+	    Article::create([
+		    'name' => 'Blog post 4',
+		    'text' => '<p>4 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
+		    'img' => 'pic4.jpg'
+	    ]);
+    }
+}
